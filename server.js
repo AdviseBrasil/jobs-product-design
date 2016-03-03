6f9164316e7bea5e93d7f1db7f9c7778d28f81d8
Higor Moreno Ribeiro <higormorenoribeiro@gmail.com>
2016-03-03 10:46:57 -0300

    correção

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var	app 	= express();
 var path	= require('path');
 var gulp 	= require('gulp');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req,res){
 	res.sendFile(path.join(__dirname+'/index.html'));
 });
