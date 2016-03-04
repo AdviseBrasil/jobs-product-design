c0e1a3f3606b0204cfe1271f989202f25d3da864
felipealvares <felipe.alvares@advise.local>
2016-03-04 09:01:51 -0300

    update another time

--- util.js
+++ util.js
@@ -21,25 +21,35 @@ util.montarHtml = function (contato){
 	var html = '<table>'
 			+ '<tr>'
 				+ '<td>Nome: </td> <td>'+ contato.nome +'</td>'
-
+			+ '</tr>'
+			+ '<tr>'
 				+ '<td>Sobrenome: </td> <td>' + contato.sobrenome + '</td>'
-
+			+ '</tr>'
+			+ '<tr>'
 				+ '<td>Email: </td> <td> '+ contato.email +' </td>'
-
+			+ '</tr>'
+			+ '<tr>'
 				+ '<td>Telefone: </td> <td> '+ contato.telefone +' </td>'
-
+			+ '</tr>'
+			+ '<tr>'
 				+ '<td>Celular: </td> <td> '+ contato.celular +' </td>'
-
+			+ '</tr>'
+			+ '<tr>'
 				+ '<td>Portifolio: </td> <td> '+ contato.portifolio +' </td>'
-
+			+ '</tr>'
+			+ '<tr>'
 				+ '<td>Linkedin: </td> <td> '+ contato.linkedin +' </td>'
-
+			+ '</tr>'
+			+ '<tr>'
 				+ '<td>Skype: </td> <td> '+ contato.skype +' </td>'
-
+			+ '</tr>'
+			+ '<tr>'
 				+ '<td>Website: </td> <td> '+ contato.website +' </td>'
-
+			+ '</tr>'
+			+ '<tr>'
 				+ '<td>Pretensão: </td> <td> '+ contato.pretensao +' </td>'
-
+			+ '</tr>'
+			+ '<tr>'
 				+ '<td>Descricao: </td><td> '+ contato.descricao +' </td>'
 			+ '</tr>'
 		+ '</table>';
