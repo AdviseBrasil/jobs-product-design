5deb673f297cc9bbf231704d40dadfa74331fba8
felipealvares <felipe.alvares@advise.local>
2016-03-04 17:01:07 -0300

    update

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -17,6 +17,39 @@
 			});
 		}
 
+		$('#contact-form').on('submit', function(event) {
+
+			event.preventDefault();
+
+			var data = $(this).serialize(),
+				_this = $(this);
+
+			if($(this).validate('isvalid')) {
+				$.ajax({
+					type: 'POST',
+					action: '/',
+					data: data,
+					beforeSend: function() {
+						_this.addClass('sending');
+					},
+					success: function(response) {
+						if(response) {
+							$('main').remove();
+							$('.sucess-contact').addClass('--show');
+						} else {
+							console.log('deu ruim');
+						}
+					}
+				});
+			}
+
+		});
+
 	});
+
+	// Mask
+	$('[name="pretensao"]').mask('000.000', {reverse: true});
+	$('[name="telefone"] , [name="celular"]').mask('(00) 0000-0000');
+
 
 })(jQuery, this);
